3726474f42542d999aecc8d15854459882b20866
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-10-26 10:07:00 +0200

    types ts mes couleurs

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,13 @@ export interface Montre {
   boitier?: "white";
   bracelets?: "black";
 }
+
+export const colors = {
+  "#000000": "Noir",
+  "#FFFFFF": "Blanc",
+  "#A8B7B2": "BleuCactus",
+  "#0B052C": "BleuNuit",
+  "#E2D8CF": "Creme",
+  "#F9DDDA": "Rose",
+  "#B5B5B5": "GrisSideral",
+};
